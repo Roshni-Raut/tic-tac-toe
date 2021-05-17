1bf7e0ffb5fa3a1592da284b177bb403f24ec6cd
roshniraut <roshniraut2000@gmail.com>
2021-05-17 18:29:49 +0530

    created board and squre components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-const App=()=>{
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+import Board from "./components/Board"
+const App= () =>{
+  return(
+    <>
+      <h1> TIC TAC TOE </h1>
+      <Board/>
+    </>
+  )
 }
 export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = (value) => {
+    return (
+        <div>
+            <button type="button">{value}</button>
+        </div>
+    )
+}
+
+export default Square
